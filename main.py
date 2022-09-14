7708c14c968eb39aded47cb8e5bd4d4955726be0
Vadim Titskiy <titskiy@lan.rowen.ru>
2022-09-14 16:24:51 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class Hello(App):
+    def build(self):
+        btn = Button(text='Hello World')
+        return btn
+
+
+Hello().run()
